4fb1df87d0616f43f9522b75e57b7583722fa457
Alasdair Smith <alasdairsmith0491@gmail.com>
2020-07-30 13:55:04 +0100

    --wip-- [skip ci]

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,10 +1,13 @@
 //import Example from "../src/example";
 import fizzbuzz from "../src/fizzbuzz";
 describe('fizzbuzz', () => {
-    it("Should take 0 and reteurn 0", () => {
-        // const example = Example();
-        //expect(example.doAThing()).toEqual(true);
+    it("Should take 0 and return 0", () => {
         const output = fizzbuzz(0);
         expect(output).toMatch('0');
+    });
+
+    it("Should take 1 and return 1", () => {
+        const output = fizzbuzz(1);
+        expect(output).toMatch('1');
     });
 });
