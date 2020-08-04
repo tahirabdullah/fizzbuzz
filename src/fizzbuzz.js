208c6a353ba1a476dc94d0b654aa44223b0b46f9
Tahir Abdullah <tahir.abdullah@digitaltheatre.com>
2020-08-04 09:54:35 +0100

    Fizzbuzz returns Fizz for 9

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,13 +1,5 @@
 export default function fizzbuzz(input){
-    if (input == 3) {
-        return 'Fizz';
-    }
-
-    if (input == 6) {
-        return 'Fizz';
-    }
-
-    if (input == 9) {
+    if (input%3 == 0) {
         return 'Fizz';
     }
 
